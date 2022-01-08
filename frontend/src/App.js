90b86f139da580ad7b5dbdd41abf70257bbf747a
Alex M <1maltsevalex@gmail.com>
2022-01-08 20:05:37 +0200

    002-React-Bootstrap Setup, Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
- return <>Hello</>;
+ return (
+  <>
+   <Header />
+   <main className='py-3'>
+    <Container>Hello</Container>
+   </main>
+   <Footer />
+  </>
+ );
 };
 
 export default App;
