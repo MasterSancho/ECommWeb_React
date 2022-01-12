495e0f09d33117ddfb78c64e7964d1007bdbc82f
Alex M <1maltsevalex@gmail.com>
2022-01-12 10:06:31 +0200

    029-User Login Screen & Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
  return (
@@ -13,6 +14,7 @@ const App = () => {
     <Container>
      <Routes>
       <Route path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
      </Routes>
     </Container>
