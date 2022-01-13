0d505cb0f752077498b44b9f2ba6a487f07edb13
Alex M <1maltsevalex@gmail.com>
2022-01-13 09:35:09 +0200

    035-Header -> LinkContainer Products

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -43,6 +43,14 @@ const Header = () => {
          </Nav.Link>
         </LinkContainer>
        )}
+
+       {userInfo && userInfo.isAdmin && (
+        <NavDropdown title='Admin' id='adminmenu'>
+         <LinkContainer to='admin/productlist'>
+          <NavDropdown.Item>Products</NavDropdown.Item>
+         </LinkContainer>
+        </NavDropdown>
+       )}
       </Nav>
      </Navbar.Collapse>
     </Container>
