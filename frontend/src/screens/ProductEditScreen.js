483df802c370b5e3fe46411cbf56fa91a2fd4b80
Alex M <1maltsevalex@gmail.com>
2022-01-15 12:19:05 +0200

    043-Front End Image Upload

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
@@ -22,6 +23,7 @@ const ProductEditScreen = () => {
  const [category, setCategory] = useState('');
  const [countInStock, setCountInStock] = useState(0);
  const [description, setDescription] = useState('');
+ const [uploading, setUploading] = useState(false);
 
  const productDetails = useSelector((state) => state.productDetails);
  const { loading, error, product } = productDetails;
@@ -51,6 +53,29 @@ const ProductEditScreen = () => {
    }
   }
  }, [successUpdate, dispatch, navigate, product, productId]);
+
+ const uploadFileHandler = async (e) => {
+  const file = e.target.files[0];
+  const formData = new FormData();
+  formData.append('image', file);
+  setUploading(true);
+
+  try {
+   const config = {
+    headers: {
+     'Content-Type': 'multipart/form-data',
+    },
+   };
+
+   const { data } = await axios.post('/api/upload', formData, config);
+
+   setImage(data);
+   setUploading(false);
+  } catch (error) {
+   console.error(error);
+   setUploading(false);
+  }
+ };
 
  const submitHandler = (e) => {
   e.preventDefault();
@@ -111,6 +136,14 @@ const ProductEditScreen = () => {
         placeholder='Enter image url'
         value={image}
         onChange={(e) => setImage(e.target.value)}></Form.Control>
+       <Form.Control
+        type='file'
+        id='image-file'
+        label='Choose File'
+        custom
+        onChange={uploadFileHandler}></Form.Control>
+
+       {uploading && <Loader />}
       </Form.Group>
 
       <Form.Group controlId='brand'>
