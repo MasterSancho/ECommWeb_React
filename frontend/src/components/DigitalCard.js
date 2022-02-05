dd21123ad2455a058a025b2ab46b196bdc0cd366
Alex M <1maltsevalex@gmail.com>
2022-02-05 12:02:13 +0200

    063-> ProfileCard

--- frontend/src/components/DigitalCard.js
+++ frontend/src/components/DigitalCard.js
@@ -3,10 +3,20 @@ import { Row, Image } from 'react-bootstrap';
 const DigitalCard = ({ digitalcard }) => {
  return (
   <>
-   <Row className='align-items-center'>
+   <Row className='align-items-center text-center'>
     <Image className='w-100' src={digitalcard.showcase} alt='showcase' fluid />
+
+    <Image
+     src={digitalcard.profile_image}
+     className='rounded-circle w-50 m-auto my-3'
+     alt=''
+     fluid
+    />
+
+    <h2 className='text-white mb-3'>{digitalcard.name}</h2>
+
+    <p className='text-white'>{digitalcard.slogan}</p>
    </Row>
-   <h2>{digitalcard.name}</h2>
   </>
  );
 };
--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,37 +1,21 @@
-import { useEffect } from 'react';
+import { Container, Row, Image } from 'react-bootstrap';
 
-import { Card } from 'react-bootstrap';
-
-const ProfileCard = ({ digitalcard }) => {
- useEffect(() => {
-  console.log(digitalcard);
- }, []);
-
+const ProfileCard = () => {
  return (
-  <Card className='my-3 p-3 rounded'>
-   <Card.Img src={digitalcard.image} />
+  <Container className='text-center'>
+   <Row>
+    <Image
+     src='images/08.09.20_Arieli_Shay_029-min.jpg'
+     className='rounded-circle w-50 m-auto my-3'
+     alt=''
+     fluid
+    />
 
-   <Card.Body>
-    <Card.Title as='div'>
-     <strong>{digitalcard.name}</strong>
-    </Card.Title>
-   </Card.Body>
+    <h2 className='text-white mb-3'>שי אריאלי</h2>
 
-   {/* <Container className='text-center'>
-    <Row>
-     <Image
-      src='images/08.09.20_Arieli_Shay_029-min.jpg'
-      className='rounded-circle w-50 m-auto my-3'
-      alt=''
-      fluid
-     />
-
-     <h2 className='text-white mb-3'>שי אריאלי</h2>
-
-     <p className='text-white'>יועץ הנדל"ן האישי שלך</p>
-    </Row>
-   </Container> */}
-  </Card>
+    <p className='text-white'>יועץ הנדל"ן האישי שלך</p>
+   </Row>
+  </Container>
  );
 };
 
