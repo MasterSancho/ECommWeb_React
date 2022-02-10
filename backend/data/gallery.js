a2434410869ea74cef4d453f6391c00c9efbe78b
Alex M <1maltsevalex@gmail.com>
2022-02-10 18:56:54 +0200

    073-> Project Gallery for Product, data

---
+++ backend/data/gallery.js
@@ -0,0 +1,60 @@
+const gallery = [
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_001-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_002-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_003-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_004-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_005-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_006-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_007-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_008-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_009-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_010-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_011-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_012-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_013-min.jpeg',
+  name: 'תמונה',
+ },
+ {
+  image: '/images/pictures_galery/08.09.20_Arieli_Shay_014-min.jpeg',
+  name: 'תמונה',
+ },
+];
+
+export default gallery;
